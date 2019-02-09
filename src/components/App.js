74417f29ef898c1237bec27be90099ff3ab7aeb7
leamsigc <leamsigc@hotmail.com>
2019-02-09 15:06:22 -0500

    add styles for the header element

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Header from './Header';
 export default class App extends Component {
 	render() {
 		return (
 			<div>
+				<Header />
                 
-				<h1>the app</h1>
 			</div>
 		);
 	}
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import headerBackgroundimg from '../images/leamsigcHomeBG.svg';
+export default function Header() {
+	return (
+		<div style={HeaderContainerStyle}>
+			<header style={headerStyle}>
+				<div className="logo__container">
+					Leam<span className="dark-letters">sigc</span>
+				</div>
+				<div className="container">
+					<h1 className="title">
+						Front & Back
+						<br /> End <br /> <span className="letter-color">D</span>eveloper <br />
+						<p className="text-small">
+							<br /> Need a website or a simple fix for your problem?
+							<br /> Get a beautiful and functional website
+							<br /> With you’r business goals on mint.
+						</p>
+					</h1>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+const headerStyle = {
+	background: `url(${headerBackgroundimg})`,
+	backgroundAttachment: 'fixed',
+	backgroundPosition: 'center',
+	backgroundSize: 'cover',
+	backgroundRepeat: 'no-repeat'
+};
+const HeaderContainerStyle = {
+	background: 'linear-gradient(to right ,#625ACA,#AB5CC6)'
+};
