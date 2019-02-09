df5caf7614fd797d4c6714b212134b301df2e803
leamsigc <leamsigc@hotmail.com>
2019-02-09 00:07:05 -0500

    parcel and reactjs add the sass file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './sass/index.sass';
+
+ReactDOM.render(<App />, document.querySelector('#app'));
