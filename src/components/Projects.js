18bc60baac185922f49569d2594a6c40e0250a9d
leamsigc <leamsigc@hotmail.com>
2019-02-15 02:08:05 -0500

    add the fist project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import gifImage from '../images/ezgif.com-optimize.gif';
 export default class Projects extends Component {
 	render() {
 		return (
@@ -7,8 +7,15 @@ export default class Projects extends Component {
 				<div className="container">
 					<div className="portfolio__item">
 						<p className="portfolio__item--role">Solo Developer &#183; UX/UI Designer</p>
-						<h4 className="portfolio__item--name">SmartCell Repair</h4>
-						<div className="portfolio__item--img" />
+						<h4 className="portfolio__item--name">SmartCell Repair Website</h4>
+						<div className="btn-container">
+							<a href="#" className="btn btn-main btn-secondary">
+								Learn More
+							</a>
+						</div>
+						<div className="portfolio__item--img">
+							<img src={gifImage} className="portfolio__item--img___gif" />
+						</div>
 					</div>
 				</div>
 			</div>
